6a18d7133f532fd90445700667f7387c48f653ac
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:31:57 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/flywaydb/spring/boot/ext/FlywayModularizedMigrationInitializer.java
+++ src/main/java/org/flywaydb/spring/boot/ext/FlywayModularizedMigrationInitializer.java
@@ -25,7 +25,7 @@ import org.springframework.util.Assert;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class FlywayModularizedMigrationInitializer implements InitializingBean, Ordered {
 
