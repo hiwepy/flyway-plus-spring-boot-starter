26c7c04c51d227e1fd7053696fab80dd3f862d28
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:31:29 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/flywaydb/spring/boot/FlywayModularizedAutoConfiguration.java
+++ src/main/java/org/flywaydb/spring/boot/FlywayModularizedAutoConfiguration.java
@@ -38,7 +38,7 @@ import java.util.stream.Collectors;
 
 /**
  * 扩展Flyway实现，解决使用Druid数据源时执行SQL权限问题（Druid安全机制导致）
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @Configuration
 @ConditionalOnClass(Flyway.class)
